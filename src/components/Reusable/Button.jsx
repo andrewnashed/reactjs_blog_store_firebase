d156c6e218196f896323991ad20db52d66568d76
Andrew <cyberandrew30@yahoo.com>
2022-03-02 10:10:12 -0500

    adding style variants to reusable Button component

--- src/components/Reusable/Button.jsx
+++ src/components/Reusable/Button.jsx
@@ -1,7 +1,30 @@
-const Button = ({text}) => {
-    return ( <button className="bg-purple-500 hover:bg-purple-700 text-2xl py-2 px-4 text-slate-100 rounded-lg">
+import { useState } from "react";
+import PropTypes from 'prop-types';
+
+
+const Button = ({text, variant, disabled}) => {
+   const [state] = useState({
+       'outline':'border-2 text-purple-500 border-purple-500 hover:bg-purple-500 hover:text-slate-100',
+       'default': 'bg-purple-500 hover:bg-purple-700',
+       'text-only': 'text-purple-500 hover:text-purple-700',
+       'disabled': 'bg-purple-300 text-purple-700'
+   })
+
+    return ( <button disabled={disabled} className={`${state[variant]} text-2xl py-2 px-4 text-slate-100 rounded-lg hover:transition duration-300 ease-in-out`}>
         {text}
     </button> );
 }
- 
+
+Button.defaultProps = {
+    disabled: false,
+    variant: 'default',
+    text: 'text'
+}
+
+Button.propTypes = {
+    disabled: PropTypes.bool,
+    variant: PropTypes.string,
+    texr: PropTypes.string.isRequired
+}
+
 export default Button;
